0035d2bb51aac5dff3ea76211ed99f1cac7aecf5
maksim.dergunov <maksim.dergunov@restream.ru>
2024-07-21 23:27:23 +0400

    фикс тестов

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -18,13 +18,14 @@ export interface ICreateUser {
   phone: string
 }
 
-export interface IUpdateUser {
-  first_name: string,
-  second_name: string,
-  display_name: string,
-  login: string,
-  email: string,
-  phone: string
+export interface IUpdateUser extends IUser {
+  id: number;
+  first_name: string;
+  second_name: string;
+  display_name: string;
+  login: string;
+  email: string;
+  phone: string;
 }
 
 export interface ILoginUser {
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -17,5 +17,5 @@ export {
   ILink,
   IDialog,
   IImage,
-  IMessage
+  IMessage,
 };
