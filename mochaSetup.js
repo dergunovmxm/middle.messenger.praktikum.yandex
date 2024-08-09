96a9f75a89b90875a6194aed3747b2f811726568
maksim.dergunov <maksim.dergunov@restream.ru>
2024-08-09 10:21:36 +0400

    фикс тестов

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,7 +1,6 @@
 import { JSDOM } from 'jsdom';
 
 const jsdom = new JSDOM('<body></body>', {});
-
 global.window = jsdom.window;
 global.XMLHttpRequest = jsdom.window.XMLHttpRequest;
 global.DOMParser = jsdom.window.DOMParser;
